edbc715b22ce01d32dfe9cfd886686822499be64
Rob Thuleen <rob.thuleen@gmail.com>
2016-02-16 09:16:06 -0800

    Cleanded up bot and user creation error handling

--- server/bot/index.js
+++ server/bot/index.js
@@ -1,16 +1,49 @@
 var Slack = require('slack-client');
 var Brain = require('./brain.js');
 
+
+
+function onOpen () {
+	var slack = this.slack;
+	var unreads = slack.getUnreadCount();
+	console.log('Welcome to Slack. You are @%s of %s', slack.self.name, slack.team.name);
+	console.log('You have %s unread ' + (unreads === 1 ? 'message' : 'messages'), unreads);
+}
+
+function onMessage (message) {
+
+	var slack = this.slack;
+
+	/// console.log("activy = " + JSON.stringify(message))
+
+	var type = message.type;
+	var channel = slack.getChannelGroupOrDMByID(message.channel);
+	var user = slack.getUserByID(message.user);
+
+	var time = message.ts;
+	var text = message.text;
+
+	var botMessage = ((type === 'message') && (text.split(' ')[0] === '<@' + slack.self.id + '>'))
+	if (botMessage) {
+		this.brain.exec(message, channel, function (response) {
+			channel.postMessage(response);
+		});
+	}
+}
+
+function onUserChange (user) {
+	console.log("User Changed: " + user.name)
+}
+
+function onError (error) {
+		console.error('Error: %s', error);
+}
+
 var Bot = {
 
 	brain: new Brain(),
 
-	sendActivationKey: function (user, key) {
-		var message = "To activate your SDE Intel Web account click here: " + key;
-		this.sendDM(user, message);
-	},
-
-	sendDM: function (user, message) {
+	sendDM: function (user, message, cb) {
 		var params = {
 			"unfurl_links": false,
 			"unfurl_media": false,
@@ -19,10 +52,13 @@ var Bot = {
 		    "text": message,
 		    "username":  this.slack.self.name
 			}
-
 		this.slack._apiCall("chat.postMessage", params, function (res) {
-			console.log("API call returned");
-			console.log("Response: " + JSON.stringify(res))
+			console.log("DM Response: " + JSON.stringify(res));
+			var err = null;
+			if (!res.ok) {
+				err = res.error
+			}
+			cb(err);
 		});
 	},
 
@@ -32,67 +68,12 @@ var Bot = {
 		var slack = new Slack(token, autoReconnect, autoMark);
 		this.slack = slack;
 
-		slack.on('open', function() {
-
-			var channels = [],
-			    groups = [],
-			    unreads = slack.getUnreadCount(),
-			    key;
-
-			for (key in slack.users) {
-				var user = slack.getUserByID(key)
-				console.log("user name = " + user.name + " user email = " + user.profile.email);
-			};
-			console.log("ROB  = " + slack.getUserByEmail("rob.thuleen@gmail.com"));
-			console.log("nobody = " + slack.getUserByEmail("who@who.com"));
-			for (key in slack.channels) {
-				if (slack.channels[key].is_member) {
-					channels.push('#' + slack.channels[key].name);
-				}
-			}
-
-			for (key in slack.groups) {
-				if (slack.groups[key].is_open && !slack.groups[key].is_archived) {
-					groups.push(slack.groups[key].name);
-				}
-			}
-
-			console.log('Welcome to Slack. You are @%s of %s', slack.self.name, slack.team.name);
-			console.log('You are in: %s', channels.join(', '));
-			console.log('As well as: %s', groups.join(', '));
-			console.log('You have %s unread ' + (unreads === 1 ? 'message' : 'messages'), unreads);
-		}.bind(this));
-
-		slack.on('message', function(message) {
-
-			console.log("activy = " + JSON.stringify(message))
-			var type = message.type;
-			var channel = slack.getChannelGroupOrDMByID(message.channel);
-			var user = slack.getUserByID(message.user);
-			console.log("USER = " + message.user);
-			var time = message.ts;
-			var text = message.text;
-			var upload = ((message.upload) && 
-						  (message.file.initial_comment) &&
-						  (message.file.initial_comment.comment.split(' ')[0] === '<@' + slack.self.id + '>'))
-			//console.log('Received: %s %s @%s %s "%s"', type, (channel.is_channel ? '#' : '') + channel.name, user.name, time, text);
-			if (
-				(type === 'message') && 
-				((text.split(' ')[0] === '<@' + slack.self.id + '>') || upload)
-				) {
-				var _this = this;
-				message.text = upload?"@" + slack.self.id + "> upload":message.text;
-				this.brain.exec(message, channel, function (response) {
-					channel.postMessage(response);
-					console.log('@%s responded with "%s"', slack.self.name, response);
-				});
-			}
-		}.bind(this));
-
-		slack.on('error', function(error) {
-
-			console.error('Error: %s', error);
-		}.bind(this));
+		// Set up event handlers
+		slack
+			.on('open', onOpen.bind(this))
+			.on('message', onMessage.bind(this))
+			.on('error', onError.bind(this))
+			.on('userChange', onUserChange.bind(this));
 
 		slack.login();
 	}
