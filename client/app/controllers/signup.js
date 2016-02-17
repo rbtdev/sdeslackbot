55951a4c68fee53af3d29c087b7b52fee47fc3d5
Rob Thuleen <rob.thuleen@gmail.com>
2016-02-16 19:23:08 -0800

    Added signup validation on front end

--- client/app/controllers/signup.js
+++ client/app/controllers/signup.js
@@ -1,22 +1,46 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+	email: "",
+	password: "",
+	verifypw: "",
+
+	hasEmail: Ember.computed.notEmpty('email'),
+	hasPassword: Ember.computed.notEmpty('password'),
+	pwIsStrong: Ember.computed.match('password', /^(?=.*\d).{4,12}$/),
+
+	pwIsEqual: function () {
+		return (this.get('password') === this.get('verifypw'))
+	}.property('password', 'verifypw'),
+
+	passwordIsValid: function () {
+		return (this.get('hasPassword') && this.get('pwIsEqual') && this.get('pwIsStrong'));
+	}.property('hasPassword', 'pwIsEqual'),
+
+	isReady: function () {
+		return (this.get('hasEmail') && this.get('passwordIsValid'));
+	}.property('hasEmail', 'passwordIsValid'),
+
+
 	actions: {
 	    signup: function() {
-	    	var _this = this;
-	    	var flashQueue = Ember.get(this, 'flashMessages');
-			var user = this.store.createRecord('user', {
-				email: this.get('email'),
-				password: this.get('password'),
-				name: this.get('name'),
-				avatar: this.get('avatar')
-			});
-			user.save().then (function () {
-				_this.transitionToRoute('success');
-			},
-			function () {
-				flashQueue.alert('Unable to create user');
-			});
+	    	if (this.get('isReady')) {
+		    	var _this = this;
+		    	var flashQueue = Ember.get(this, 'flashMessages');
+				var user = this.store.createRecord('user', {
+					email: this.get('email'),
+					password: this.get('password'),
+					name: this.get('name'),
+					avatar: this.get('avatar')
+				});
+				user.save().then (function () {
+					_this.transitionToRoute('success');
+				},
+				function () {
+					flashQueue.alert('Unable to create user');
+				});
+			}
 		}
 	}
 });
