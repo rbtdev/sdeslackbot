ac0134a748dec4e69775a98527179cb4ae2ce3e9
Rob Thuleen <rob.thuleen@gmail.com>
2016-02-19 11:21:57 -0800

    Upload working

--- middleware/csvUpload.js
+++ middleware/csvUpload.js
@@ -1,28 +1,24 @@
 var path = require('path');
 var fs = require('fs');
 
-function csvUpload(modelName) {
+function  csvUpload(modelName, fieldNames) {
   return function (req, res, next) {
-    var Converter = require("csvtojson").core.Converter;
-    var converter = new Converter({constructResult:true});
-    console.log('Converting ' + req.file.filename);
-    console.log('File Mimetype ' + req.file.mimetype);
+    var Converter=require("csvtojson").Converter;
+    var converter= new Converter({headers: fieldNames});
+    console.log("Field Names:" + fieldNames);
+
     converter.on("end_parsed",
           function(jsonObj) {
-            console.log(modelName + ":" + jsonObj);  
             req.body[modelName] = jsonObj;
             next();
-      });
+    });
+
     converter.on("error",function(errMsg,errData){
-      console.log("Converter error: " + errMsg);
-      console.log("Error Data: " + JSON.stringify(errData))
       res.status(500).send({msg:errMsg, data: errData})
     });
-    console.log('Building path...');
+
     var filePath = path.join(__dirname, '../public/uploads/',  req.file.filename);
-    console.log('filePath = ' + filePath);
-    var readFile = fs.createReadStream(filePath);
-    readFile.pipe(converter);
+    var readFile = fs.createReadStream(filePath).pipe(converter);
   }
 }
 
--- models/location.js
+++ models/location.js
@@ -4,13 +4,46 @@ var mongoose = require('mongoose');
 var locationSchema = new mongoose.Schema({
     name: String,
     area: String,
+    lat: Number,
+    lng: Number,
     intelUrl: String,
     mapsUrl: String,
     shortCode: String,
 	//author: { type: mongoose.Schema.ObjectId, ref: 'user' }
 });
 
+function _preSave(next) {
+	console.log("Inside pre-save. Location: " + this.name)
+	if (!this.name || !this.intelUrl || !this.area) {
+		return next (new Error());
+	}
+	if ((this.name.length == 0) || (this.intelUrl.length ==0) || (this.area.length == 0)) {
+		return next (new Error());
+	}
+
+	/// Passed basic validation
+
+	var params = this.intelUrl?this.intelUrl.split("pll="):[]
+	var llStr = params.length==2?params[1]:"0,0";
+	var ll = llStr.split(',');
+	this.lat = parseFloat(ll[0]);
+	this.lng = parseFloat(ll[1]);
+	this.mapsUrl = "http://maps.google.com/?q=" + this.name + "@" + this.lat + "," + this.lng;
+	next();
+};
+
+function preSave(location, next) {
+	_preSave.bind(location)(next);
+}
+
+locationSchema.pre('save', _preSave);
+
 //locationSchema.plugin(acl.object);
 
 var LocationModel = mongoose.model('location',locationSchema);
-module.exports = LocationModel;
+
+module.exports = {
+	model: LocationModel,
+	preSave: preSave,
+	fieldNames: ["name", "area", "intelUrl", "mapsUrl", "shortCode"]
+};
