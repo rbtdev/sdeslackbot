0787a3e25e91ba4c7448e9f928d327b4ce9e12b5
Rob Thuleen <rob.thuleen@gmail.com>
2016-03-02 15:39:27 -0800

    Create locations from outgress alerts

--- server/controllers/alert.js
+++ server/controllers/alert.js
@@ -1,4 +1,5 @@
 var AlertModel = require('../models/alert.js');
+var LocationModel = require('../models/location.js');
 
 function create(req) {
 	console.log('body:' + JSON.stringify(req.body))
@@ -11,7 +12,14 @@ function create(req) {
 		portal_url:  req.body['portal_url'],
 		attacker_url: req.body['attacker_url']
 	};
-	console.log("Creating alert: " + JSON.stringify(alert));
+	var location = new LocationModel({
+		intelUrl: alert.portal_url,
+		name: alert.portal_name,
+		area: "unknown",
+		shortCode: "",
+		method: "outgress"
+	});
+	location.save();
 	return alert;
 }
 module.exports = {
