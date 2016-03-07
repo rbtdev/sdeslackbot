169a7d7de648dfb02b127f4868a8b677a64ef3a7
Rob Thuleen <rob.thuleen@gmail.com>
2016-03-07 08:26:00 -0800

    Added range commmand

---
+++ bot/commands/range/index.js
@@ -0,0 +1,144 @@
+multiplier = {
+  la: 2,
+  ula: 5,
+  vrla: 7
+}
+
+function level(portal) {
+  var levels   = portal.split('');
+  var total = 0;
+  levels.forEach(function (level) {
+    total += parseInt(level)
+  })
+  return Math.floor(total/8);
+}
+
+
+function linkRange(average, linkamps) {
+    linkamps.sort(function (a, b) {
+      multiplier[b] - multiplier[a]
+    })
+    baseRange   = 160*Math.pow(average, 4)
+    return baseRange*rangeBoost(linkamps);
+
+  function rangeBoost(linkamps) {
+    scale = [1.0, 0.25, 0.125, 0.125];
+    boost = 0.0;
+    count = 0;
+    linkamps.forEach(function (mod) {
+      if (multiplier[mod]) {
+        baseMultiplier = multiplier[mod];
+        boost += baseMultiplier*scale[count]
+        count++
+      }
+    })
+    if (count > 0) {
+      return boost;
+    } 
+    else {
+      return 1.0
+    }
+  }
+}
+
+function metric (range) {
+  if (range >= 10000)
+    range = Math.round(range/10)/100 + "km"
+  else if (range >= 1000)
+    range = Math.round(range/10)/100 + "km"
+  else
+    range = Math.round(range) + "m"
+  return range
+}
+
+function miles(range) {
+  var miles = (range/1000) * 0.621371;
+  return miles.toFixed(2) + "mi";
+}
+
+function portalCalc(range) {
+
+  var maxRange = linkRange(8, ["vrla", "vrla", "vrla", "vrla"]);
+  if ((range > maxRange) || (range < 0)) {
+    return {err: "Range is too far"};
+  }
+  
+  var level = Math.ceil(Math.pow(range/160, 0.25));
+  if (level <= 8) {
+    return ({
+      level: level,
+      mods:[]
+    })
+  }
+  var mods = [];
+  var results = [];
+  var testRange = 0;
+  var level = 8;
+  while (level >=1 ) {
+    do  {
+      if (linkRange(level, mods) >= range) {
+        return ({
+          level:level,
+          mods: []
+        });
+      }
+      for (mod in multiplier) {
+        for (modSlot = 0; modSlot < 4; modSlot++) {
+          mods[modSlot] = mod;  
+          testRange = linkRange(level, mods)
+          if (testRange >= range) {
+            var resultMods = [];
+            mods.forEach(function (mod) {
+              resultMods.push(mod);
+            })
+            return ({
+              level: level,
+              mods: resultMods
+            });
+          }        
+        }
+
+      }
+    level--
+    } while (testRange < range)
+  }
+  return results
+}
+
+function range (command) {
+
+  var args = command.args._;
+  var respond = command.respond;
+  if (!args) return respond ({text: help})
+  if (!args[0]) return respond ({text: "Need valid portal or range"})
+
+  args[0] = args[0].toString();
+  if (args[0].length < 8) {
+    var result = portalCalc(parseFloat(args[0])*1000);
+    if (result.err) return respond ({text: result.err});
+    var mods = "";
+    result.mods.forEach(function (mod) {
+      mods += mod.toUpperCase() + " ";
+    })
+    var response = "Level " + result.level;
+    if (mods.length) response += " with " + mods;
+    respond({text: response})
+  }
+
+  else {
+    if (args[0].indexOf('9') > -1) return respond({text: "Need valid portal"})
+    if (args[0].length > 8) return respond({text: "Need a valid portal"})
+    var mods = args.splice(1)
+    var range = linkRange(level(args[0]), mods);
+    respond({text: "Range: " + metric(range) + " (" + miles(range) + ")"})
+  }
+}
+
+desc = "Find the link range of a portal, or minumum portal needed to link to a given range.";
+var help = "range <reso_levels> <mods> or range <distance>"
+
+module.exports = {
+  exec: range,
+  usage: help,
+  desc: desc
+}
---
+++ bot/commands/range/test.js
@@ -0,0 +1,49 @@
+var range = require("./index.js");
+
+var command = {
+	args: {_:[88888888, "vrla", "vrla", "vrla", "vrla"]},
+	respond: respond
+}
+
+function respond(text) {
+	console.log(text);
+}
+
+console.log("testing with : "+ command.args._);
+range.exec(command);
+
+var command = {
+	args: {_:[6881.28]},
+	respond: respond
+}
+console.log("testing with : "+ command.args._);
+range.exec(command);
+
+var command = {
+	args: {_:[88888889]},
+	respond: respond
+}
+console.log("testing with : "+ command.args._);
+range.exec(command);
+
+var command = {
+	args: {_:[0]},
+	respond: respond
+}
+console.log("testing with : "+ command.args._);
+range.exec(command);
+
+var command = {
+	args: {_:[10000]},
+	respond: respond
+}
+console.log("testing with : "+ command.args._);
+range.exec(command);
+
+var command = {
+	args: {_:[88888888]},
+	respond: respond
+}
+console.log("testing with : "+ command.args._);
+range.exec(command);
+
