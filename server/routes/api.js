fa3afd47a66f881a78a119910d5c43f0be452940
Rob Thuleen <rob.thuleen@gmail.com>
2017-11-19 13:07:23 -0800

    Remove alerts post api endpoint due to errors

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,7 +8,9 @@ var FileController = require('../controllers/file.js');
 var AlertController = require('../controllers/alert.js');
 var CsvUpload = require('../middleware/csvUpload.js');
 var ApiController = require('../controllers/api.js');
-var FileUpload = require('multer')({ dest: './public/uploads/'});
+var FileUpload = require('multer')({
+	dest: './public/uploads/'
+});
 var Locations = ApiController(LocationController);
 var Alerts = ApiController(AlertController);
 var api = express.Router();
@@ -27,8 +29,10 @@ api
 	// Images (uploaded avatars)
 	.get('/images/:id', FileController.getImage)
 
-	// Receive Outgress Alerts
-	.post('/alerts',FileUpload.array(), Alerts.create)
+// Receive Outgress Alerts
+// Removed. 503 happening when alerts is called from outgres. Likely a mongo error.
+// needs to be investigated
+//.post('/alerts',FileUpload.array(), Alerts.create)
 
 ;
 
@@ -51,12 +55,12 @@ api.use(AuthController.isAuthorized)
 	.get('/locations', Paginate, Locations.readAll)
 	.put('/locations/:id', Locations.updateOne)
 	.delete('/locations/:id', Locations.delete)
-	.get('/locations/download',  Locations.download)
+	.get('/locations/download', Locations.download)
 
 	// Files
 	.post('/files/', FileUpload.single('file[file]'), FileController.create)
-	.post('/locationsFiles', FileUpload.single('locationsFile[file]'), 
-		CsvUpload(LocationController.collectionName, LocationController.importFields), 
+	.post('/locationsFiles', FileUpload.single('locationsFile[file]'),
+		CsvUpload(LocationController.collectionName, LocationController.importFields),
 		Locations.updateBulk)
 
 // End of routes 
